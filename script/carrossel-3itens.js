2d8f97c83ab0d7b02346f120d37face3fd5abd82
Legenda <willian.g.h@hotmail.com>
2024-07-23 15:42:07 -0300

    Criação página balança

---
+++ script/carrossel-3itens.js
@@ -0,0 +1,18 @@
+let radio = document.querySelectorAll('.manual-btn')
+let cont = 1
+
+document.getElementById('radio1').checked = true
+
+setInterval(() => {
+    proximaImg()
+}, 5000)
+
+function proximaImg(){
+    cont++
+
+    if(cont > 3){
+        cont = 1
+    }
+
+    document.getElementById(`radio${cont}`).checked = true
+}
